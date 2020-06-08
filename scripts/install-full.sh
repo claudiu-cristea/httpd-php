8189a58d50cd86623ca5a415fdf16b5e679dabb9
Tiago Mauricio <tiago.mauricio@ext.ec.europa.eu>
2020-06-08 17:04:10 +0200

    Unpin versions from drush and composer

--- scripts/install-full.sh
+++ scripts/install-full.sh
@@ -7,11 +7,11 @@ apt-get install -y wget unzip mysql-client
 # Install composer
 wget https://getcomposer.org/installer -O /tmp/composer-install
 
-php /tmp/composer-install --install-dir=/usr/bin --filename=composer --version=${composer_version}
+php /tmp/composer-install --install-dir=/usr/bin --filename=composer
 
 # Install drush & security-checker
 ## Temporarily pin to version 8.2.3... 
-composer global require drush/drush:8.2.3 sensiolabs/security-checker
+composer global require drush/drush:^8 sensiolabs/security-checker
 # Touch up
 ln -s /usr/bin/composer /usr/local/bin/composer
 ln -s $(realpath ~/.composer/vendor/bin/drush) /usr/local/bin/drush
