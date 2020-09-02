6e2e17b2d248dd982d3dd33a73b554760af8289b
Victor Da Costa <vdacosta@voidtek.com>
2020-09-02 09:23:56 +0000

    Update install-ci.sh

--- scripts/install-ci.sh
+++ scripts/install-ci.sh
@@ -8,6 +8,20 @@ apt-get install -y ${ci_packages}
 
 adduser www-data root
 
+# Install NodeJS:
+curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+apt-get update
+apt-get install -y nodejs yarn build-essential
+
+cd /opt
+
+yarn add --cache-folder /tmp wetty.js
+
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 rm -rf /tmp/*
